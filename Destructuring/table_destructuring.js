3ad15240a824fb6004c48dbb41be99775c101b3b
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2021-09-08 00:31:34 +0200

    Table destructuring excercises done

---
+++ Destructuring/table_destructuring.js
@@ -0,0 +1,25 @@
+//Przykład 1
+
+// const tab = ["kot","pies","sowa"];
+
+// const [cos1,cos2,cos3] = tab
+
+// console.log(cos1);
+
+// console.log(cos1,cos3);
+
+// const tab = ["Ala", "Ola"];
+
+// const [ name1 = "brak", name2 = "brak", name3 = "brak" ] = tab;
+
+// console.log(name1,name2,name3)
+
+//Przykład 2
+
+let a = 1;
+let b = 5;
+
+[a, b] = [b, a];
+
+console.log(a); 
+console.log(b); 
